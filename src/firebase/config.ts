af853efd282b87f1bd47ef4fd7fb1ab08d22de2a
Isaac Anchislavsky <ianchislavsky@gmail.com>
2025-11-04 14:55:47 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-3861763439-b3374",
   "appId": "1:862962215357:web:68e2688ee852eae60ecad4",
+  "storageBucket": "studio-3861763439-b3374.appspot.com",
   "apiKey": "AIzaSyDU8PidVXd0WPRwfoGkaiQ84GRbIIUX3RU",
   "authDomain": "studio-3861763439-b3374.firebaseapp.com",
   "measurementId": "G-JDMLNNJ3JQ",
